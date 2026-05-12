35f2a4598420f685d78dba122028e97d4ca23e46
polinamolofeeva2003-a11y <polina.molofeeva2003@gmail.com>
2026-05-12 14:47:28 +0000

    Tue, May 12, 2026, 9:47 PM +07:00

---
+++ test.js
@@ -0,0 +1,4 @@
+body = document.querySelector("body")
+document.querySelector("#red").addEventListener('click', function() {body.style.backgroundColor="red";})
+document.querySelector("#blue").addEventListener('click', function() {body.style.backgroundColor="Blue";})
+document.querySelector("#green").addEventListener('click', function() {body.style.backgroundColor="green";})
